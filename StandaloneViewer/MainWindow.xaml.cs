792ce085d610adb4a2558165cd0c2d18b76779b2
Siyu Zhang <astro6974@gmail.com>
2021-03-27 13:41:17 +0800

    Add license notice

--- StandaloneViewer/MainWindow.xaml.cs
+++ StandaloneViewer/MainWindow.xaml.cs
@@ -1,4 +1,24 @@
-﻿using System;
+﻿/*    
+	QuickFits - FITS file preview plugin for QL-win
+    Copyright (C) 2021 Siyu Zhang
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+    USA
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -50,9 +70,9 @@ namespace StandaloneViewer
             InitializeComponent();
 
             //string path = "E:/temp/2020-01-02-1456_6-CapObj_0000.FIT";
-            string path = "E:/temp/3ch.fit";
-            //string path = "E:/temp/float.fit";
-            
+            //string path = "E:/temp/3ch.fit";
+            string path = "E:/temp/float.fit";
+
             //string path = "E:/temp/小房牛 M31-009B.fit";
 
             var fitsImage = FitsImageCreate(Marshal.StringToHGlobalAnsi(path));
