24493293fe2a84bbb23b6fdc6c59961e3f17782f
Siyu Zhang <astro6974@gmail.com>
2021-03-29 00:32:39 +0800

    Switch to x64

--- Scripts/pack-zip.ps1
+++ Scripts/pack-zip.ps1
@@ -1,5 +1,5 @@
 Remove-Item ..\QuickLook.Plugin.FitsViewer.qlplugin -ErrorAction SilentlyContinue
 
-$files = Get-ChildItem -Path ..\Release\ -Exclude *.pdb,*.xml
+$files = Get-ChildItem -Path ..\bin\Release\ -Exclude *.pdb,*.xml
 Compress-Archive $files ..\QuickLook.Plugin.FitsViewer.zip
-# Move-Item ..\QuickLook.Plugin.FitsViewer.zip ..\QuickLook.Plugin.FitsViewer.qlplugin
+Move-Item ..\QuickLook.Plugin.FitsViewer.zip ..\QuickLook.Plugin.FitsViewer.qlplugin
